8a2eb8b395e0027d45ad0e53d239ac083257df3b
Mohamed Melhem <melhemmohamed12@gmail.com>
2025-05-11 13:22:35 +0200

    Kérdés Osztály Kész.

--- Legyen ön is milliomos/Kerdes.cs
+++ Legyen ön is milliomos/Kerdes.cs
@@ -8,6 +8,34 @@ namespace Legyen_ön_is_milliomos
 {
 	internal class Kerdes
 	{
+		int id;
+		string helyesvalasz;
+		string kategoria;
+		List<string> valaszok;
+		string kerdesszoveg;
 
-	}
+        public Kerdes(string sor)
+        {
+            string[] elemek = sor.Split(';');
+            id = int.Parse(elemek[0]);
+            kerdesszoveg = elemek[1];
+            valaszok = new List<string>() { elemek[2] , elemek[3], elemek[4], elemek[5] };
+            helyesvalasz = elemek[6];
+            kategoria = elemek[7];
+
+        }
+
+        public override string? ToString()
+        {
+            return $"({kategoria}) Kérdés: {kerdesszoveg}\n" +
+                $"Válaszok: A: {valaszok[0]}  B: {valaszok[1]}\n" +
+                $"C: {valaszok[2]}  D: {valaszok[3]}";
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Helyesvalasz { get => helyesvalasz; set => helyesvalasz = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+        public List<string> Valaszok { get => valaszok; set => valaszok = value; }
+        public string Kerdesszoveg { get => kerdesszoveg; set => kerdesszoveg = value; }
+    }
 }
