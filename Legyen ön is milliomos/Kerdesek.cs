a52bc24c0777bb5fff23b5069c1903b58dfdf8b7
Mohamed Melhem <melhemmohamed12@gmail.com>
2025-05-11 13:48:35 +0200

    Kérdések osztály kész,.

--- Legyen ön is milliomos/Kerdesek.cs
+++ Legyen ön is milliomos/Kerdesek.cs
@@ -8,6 +8,36 @@ namespace Legyen_ön_is_milliomos
 {
 	internal class Kerdesek
 	{
+		List<Kerdes> kerdesLista = new List<Kerdes>();
+		List<Sorkerdes> sorkerdesLista = new List<Sorkerdes>();
 
-	}
+
+		Random rand = new Random();
+
+        public Kerdesek(string kerdesfajl, string sorkerdesfajl)
+        {
+            foreach (string sor  in File.ReadAllLines(sorkerdesfajl)) 
+            {
+                sorkerdesLista.Add(new Sorkerdes(sor));
+            }
+            foreach (string sor in File.ReadLines(kerdesfajl))
+            {
+                kerdesLista.Add(new Kerdes(sor));
+            }
+        }
+
+        public Sorkerdes VeletlenSorkerdesKivalasztas()
+        {
+            int index = rand.Next(sorkerdesLista.Count);
+            return sorkerdesLista[index];
+        }
+        public Kerdes VeletlenKerdesKivalasztas()
+        {
+            int index = rand.Next(kerdesLista.Count);
+            return kerdesLista[index];
+        }
+
+
+
+    }
 }
